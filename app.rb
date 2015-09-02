9364e6d43cfe1e4b7ff8166fbb21d88fcd23752b
Ivan Lavriv <ilavriv@admins-mac-mini.lv.remit.se>
2015-09-02 18:35:08 +0300

    Added app

--- app.rb
+++ app.rb
@@ -1,11 +1,23 @@
 require "sinatra/base"
 require "sinatra/reloader"
 
+class User
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
 
 class App < Sinatra::Base
   register Sinatra::Reloader
 
-  get '/users/:id' do
-    "sfdsafdsafs"
+  get '/users' do
+    "sffsfs"
+  end
+
+  post '/users' do
+    user = request.body.read
   end
 end
